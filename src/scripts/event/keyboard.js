6e147f2d18ec21b5f50fa7ba9781fa87c39f04d3
Qzr <z5021996@vip.qq.com>
2020-05-22 18:00:18 +0800

    :construction: 添加keycode

--- src/scripts/event/keyboard.js
+++ src/scripts/event/keyboard.js
@@ -8,7 +8,38 @@ function keyboardEvent(e, store) {
       store.dispatch('program/delItem', pickId)
       break
     }
+
+    case 37: {
+      let pickId = store.getters['common/getPickId']
+      if(pickId === 0) return
+      let ele = store.getters['program/getEle']
+      ele.location_x = ele.location_x - 1
+      console.log('left')
+      break
+    }
+
+    case 38: {
+      let pickId = store.getters['common/getPickId']
+      if(pickId === 0) return
+      console.log('up')
+      break
+    }
+
+    case 39: {
+      let pickId = store.getters['common/getPickId']
+      if(pickId === 0) return
+      console.log('right')
+      break
+    }
+
+    case 40: {
+      let pickId = store.getters['common/getPickId']
+      if(pickId === 0) return
+      console.log('down')
+      break
+    }
   }
 }
+
 
 export default keyboardEvent
